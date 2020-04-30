b8eadc3850e0923f05f78692a0a8bb41de2ce2a5
asmaaramadan99 <58226647+asmaaramadan99@users.noreply.github.com>
2020-04-29 17:16:12 -0700

    updated Authentication class

--- src/eg/edu/alexu/cs/datastructures/classes/Authentication.java
+++ src/eg/edu/alexu/cs/datastructures/classes/Authentication.java
@@ -1,27 +1,19 @@
-package eg.edu.alexu.cs.datastructures.classes;
-import java.io.EOFException;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import eg.edu.alexu.cs.datastructures.Interfaces.IContact;
-
-
 public class Authentication {
 	
 	protected String SystemUsersInfoPath;
 	private IContact contact;
 	boolean matchPass=false;
+	private Contact currentUser;
+	private String email;
+	private String password;
+	
 	
 	//for sign up
 	Authentication(String SystemUsersInfoPath,IContact contact )
 	{
 		this.SystemUsersInfoPath=SystemUsersInfoPath;
+		this.email=contact.getEmail();
+		this.password=contact.getPassword();
 		this.contact=contact;
 		
 	}
@@ -29,31 +21,44 @@ public class Authentication {
 	Authentication(String SystemUsersInfoPath,String email,String password)
 	{
 		this.SystemUsersInfoPath=SystemUsersInfoPath;
-		this.contact= new Contact(email,password);
+		this.email=email;
+		this.password=password;
+		
+		
 	}
-	 //sign up
-	 public Boolean exist(Contact contact) throws ClassNotFoundException, FileNotFoundException
+	/*
+	 * @return the user that successfully singed in
+	 */
+	
+	public Contact getCurrentUser()
+	{
+		return currentUser;
+	}
+	 /*checks whether the contact already exists or not
+	  * @return true if exists
+	  * false otherwise
+	  * @throws class not found & file not found exception
+	  * 
+	  */
+	 public Boolean exist() throws ClassNotFoundException, FileNotFoundException
 	{   
 
 		try {
 			ObjectInputStream read= new ObjectInputStream(new FileInputStream(SystemUsersInfoPath));
 			 while(true) {
 				Contact current=(Contact) read.readObject();
-				if(current.email.equals(contact.email)) {
-					if(current.password.equals(contact.password)) matchPass=true;
+				if(current.getEmail().equals(email)) {
+					if(current.getPassword().equals(password)) {matchPass=true; this.currentUser=current;}
 					read.close();
 					return true;
 				            }
 			   }	
 		} catch(EOFException e) 
-		{
-			return false;
+		{   
+		  return false;
 		}
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-					
+		catch (IOException e) {e.printStackTrace();}
+		
 		return false;
 		
 	    }
@@ -63,14 +68,16 @@ public class Authentication {
 		 Contact user=(Contact) contact;
 		 String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
 	     Pattern pattern = Pattern.compile(regex);
-	     boolean match=pattern.matcher(user.email).matches();
+	     boolean match=pattern.matcher(user.getEmail()).matches();
 		 return match;
 	 }
-	 //for sign up
+	 /*
+	  * add new user to system when he signs up successfully
+	  * @throws IO exception 
+	  *
+	  */
 	 public void addNewUser(boolean first)
 	 { 
-		if(isValidEmailFormat()) {
-		
 		 try {
 			 if(first) { 
 			ObjectOutputStream write= new ObjectOutputStream(new FileOutputStream(SystemUsersInfoPath));
@@ -94,8 +101,5 @@ public class Authentication {
 		}
 		
 		
-		}			 
-	 
-		else System.out.print("invalid email format");
+		}			 	
 	 }	
-     }
