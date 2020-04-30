90f7c3aaf543923e584a2a4a82d0e55d5d9f7b16
asmaaramadan99 <58226647+asmaaramadan99@users.noreply.github.com>
2020-04-29 17:18:51 -0700

    updated user class

--- src/eg/edu/alexu/cs/datastructures/classes/User.java
+++ src/eg/edu/alexu/cs/datastructures/classes/User.java
@@ -1,135 +1,55 @@
 package eg.edu.alexu.cs.datastructures.classes;
-
-
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-
 import eg.edu.alexu.csd.datastructure.*;
 import eg.edu.alexu.cs.datastructures.Interfaces.*;
 
+public class User implements IUser, Serializable {
 
-
- public class User extends myContact implements IUser, Serializable {
-	
-	
 	/*
 	 * u should use writeUserToFile method after any updates to the user
 	 */
-	
+
 	private static final long serialVersionUID = 1L;
-	String name;
-	String password;
-	String userFolderPath;
-	File folder;
+	Contact user;
 	SinglyLinkedList emails = new SinglyLinkedList();
-	SinglyLinkedList innerFolders = new SinglyLinkedList();
-	SinglyLinkedList userFoldersPaths = new SinglyLinkedList();
-	SinglyLinkedList myContacts=new SinglyLinkedList();
-	// should be changed to contact
-	
-	
+	SinglyLinkedList myContacts = new SinglyLinkedList();
+
 	// TODO: encrypt stored passwords
-	   User(String name, String email, String password) {
-		
-		// TODO: check proper format
-		super();
-		this.name = name;
-		this.emails.add(email);
-		this.password = password;
-		
+	public User(Contact user) {
+		this.user = user;
+		this.emails.add(user.getEmail());
 		store();
 	}
-	  
-	
-	
-	void store(){
-		makeInitialFiles();
+
+	public void store() {
 		writeUserToFile();
 	}
-	
-	void makeInitialFiles() {
-		
-		// create account folder
-		userFolderPath = App.accountsFolderPath + File.separator + this.name;
-		folder = new File(userFolderPath);
-		folder.mkdir();
-		
-		
-		// create inner folder which represents inbox, trash .. etc
-		String[] innerFoldersName = new String[] {"inbox","draft","trash"};
-		for (int i = 0; i < innerFoldersName.length; i++) {
-			createNewFolder(innerFoldersName[i]);
-		}
-		/*
-		 * @asmaa 
-		 * added a file for user's contacts
-		 */
-		
-		
-		File myContacts=new File(userFolderPath+File.separator+"myContacts.bin");
-		try {
-			myContacts.createNewFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-					
-	}
-	//save contacts to myContactsFile
-	public void saveContacts()
-	{
-		FileManager.writeToFile(myContacts, userFolderPath+File.separator+"myContacts.bin");
-	}
-	//load Contacts
-	SinglyLinkedList loadContacts()
-	{
-		SinglyLinkedList  l=(SinglyLinkedList) FileManager.getFile(userFolderPath+File.separator+"myContacts.bin");
-		/*
-		*	or return this.myContacts
-		*/
-		return l;
-	}
-	
-	public void createNewFolder(String folderName) {
-		UserInnerFolder tmp = new UserInnerFolder(this, folderName);
-		this.innerFolders.add( tmp );
-	}
-	
-	
+
 	void writeUserToFile() {
-		String userInfoFilePath = this.userFolderPath + File.separator + "userInfo.bin";
+		String userInfoFilePath = this.user.getUserPath() + File.separator + "userInfo.bin";
 		FileManager.writeToFile(this, userInfoFilePath);
 	}
-	
+
 	void addAnEmailToUser(String email) {
-		if(!isValidEmail(email)) // authentication class
-			return ; //TODO
-	
+		if (!isValidEmail(email))
+			return;
+
 		this.emails.add(email);
 		writeUserToFile();
 	}
 
-	 boolean isValidEmail(String email){
+	boolean isValidEmail(String email) {
 		String emailRegex = "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$";
-	    return email.matches(emailRegex);
-	 }
-	 
-	 UserInnerFolder getFolder(String folderName)  {
-		return (UserInnerFolder)(FileManager.getFile(
-				getFolderIndexFilePath(folderName)) );
-	 }
-	 
-	 String getFolderIndexFilePath(String folderName) {
-		 return userFolderPath + File.separator + folderName + File.separator + UserInnerFolder.indexFileName;
-	 }
-	 
-	 boolean folderExists(UserInnerFolder innerFolder) {
-		 return innerFolders.contains(innerFolder);
- }
-	
-	
-}
+		return email.matches(emailRegex);
+	}
 
+	boolean folderExists(String folderName) {
+
+		File tmpDir = new File(user.getUserPath() + File.separator + folderName);
+
+		return tmpDir.exists();
+	}
+
+}
